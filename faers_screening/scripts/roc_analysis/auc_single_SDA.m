872194797ee61e2fd7f65a0d0dcd4b32dc66136a
elpidakon <46748971+elpidakon@users.noreply.github.com>
2021-09-13 13:53:11 +0300

    Update auc_single_SDA.m

--- faers_screening/scripts/roc_analysis/auc_single_SDA.m
+++ faers_screening/scripts/roc_analysis/auc_single_SDA.m
@@ -1,12 +1,15 @@
-% Kontsioti, Maskell, Dutta & Pirmohamed, A reference set of clinically relevant adverse drug-drug interactions (2021)
-% This script calculates AUC scores for the single-drug signal detection algorithms (i.e. PRR, EBGM, BCPNN)
+% Kontsioti, Maskell, Dutta & Pirmohamed, A reference set of clinically
+% relevant adverse drug-drug interactions (2021) This script calculates AUC
+% scores for the single-drug signal detection algorithms (i.e. PRR, EBGM,
+% BCPNN)
 
 rng default;
 
 stats = {'PRR','EBGM','Q_0025logIC'};
 
 % Resource union
-union_clt_tbl = singledrugresourceunionscores(:,{'No','SOURCE','PRR','EBGM','Q_0025logIC','indicator', 'N'});
+union_clt_tbl = singledrugresourceunionscores(:,{'No','SOURCE','PRR', ...
+    'EBGM','Q_0025logIC','indicator', 'N'});
 
 union_AUC_vector = zeros(size(stats,1),3);
 
@@ -17,12 +20,14 @@ for k=1:length(stats)
     curstat=stats{k};
     scores = table2array(union_clt_tbl(:,curstat));
     scores_cell = num2cell(scores);
-    [X,Y,T, AUC] = perfcurve(label_cell,scores_cell,1,'XVals',[0:0.05:1], 'Alpha',0.05, 'BootType', 'student');
+    [X,Y,T, AUC] = perfcurve(label_cell,scores_cell,1,'XVals', ...
+        [0:0.05:1], 'Alpha',0.05, 'BootType', 'student');
     union_AUC_vector(k,:) = AUC;
 end
 
 % Resource intersection
-intersect_clt_tbl = singledrugresourceintersectscores(:,{'No','SOURCE','PRR','EBGM','Q_0025logIC','indicator', 'N'});
+intersect_clt_tbl = singledrugresourceintersectscores(:,{'No','SOURCE', ...
+    'PRR','EBGM','Q_0025logIC','indicator', 'N'});
 
 intersect_AUC_vector = zeros(size(stats,1),3);
 
@@ -33,6 +38,7 @@ for k=1:length(stats)
     curstat=stats{k};
     scores = table2array(intersect_clt_tbl(:,curstat));
     scores_cell = num2cell(scores);
-    [X,Y,T, AUC] = perfcurve(label_cell,scores_cell,1,'XVals',[0:0.05:1], 'Alpha',0.05, 'BootType', 'student');
+    [X,Y,T, AUC] = perfcurve(label_cell,scores_cell,1,'XVals',[0:0.05:1], ...
+        'Alpha',0.05, 'BootType', 'student');
     intersect_AUC_vector(k,:) = AUC;
 end
