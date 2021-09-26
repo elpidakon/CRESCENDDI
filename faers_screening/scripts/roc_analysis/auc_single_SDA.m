24bbeb67f6b5798da3b5df200c9c0742c4d3a662
elpidakon <E.Kontsioti@liverpool.ac.uk>
2021-09-26 23:56:40 +0200

    code reformatting

--- faers_screening/scripts/roc_analysis/auc_single_SDA.m
+++ faers_screening/scripts/roc_analysis/auc_single_SDA.m
@@ -40,7 +40,6 @@ for k=1:length(stats)
     scores = table2array(intersect_clt_tbl(:,curstat));
     scores_cell = num2cell(scores);
     [X,Y,T, AUC] = ...
-        perfcurve(label_cell,scores_cell, 1, ...
-        'XVals', [0:0.05:1], 'Alpha', 0.05, 'BootType', 'student');
+    perfcurve(label_cell,scores_cell, 1, 'XVals', [0:0.05:1], 'Alpha', 0.05, 'BootType', 'student');
     intersect_AUC_vector(k,:) = AUC;
 end
